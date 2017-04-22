11dcfe1bf7259cd26b87216546719013ca3710b0
Nicholas Bolt <nicholas.bolt@northgateps.com>
2017-04-22 14:28:35 +0100

    :sparkles: Add ebook to newsletter signup

--- app/controllers/newsletter_signups_controller.rb
+++ app/controllers/newsletter_signups_controller.rb
@@ -3,6 +3,8 @@ class NewsletterSignupsController < ApplicationController
     @newsletter_signup = NewsletterSignup.new(newsletter_signup_params)
     if @newsletter_signup.save
       @success = true
+      @pdf_url = ENV["PDF_URL"]
+      ApplicationMailer.guide(@newsletter_signup.email, @pdf_url).deliver
     else
       @success = false
     end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
+require 'open-uri'
+
 class ApplicationMailer < ActionMailer::Base
   default from: ENV["FROM_EMAIL"]
 
   layout "mailer"
+
+  def guide(email, pdf_url)
+    attachments['Liam Howells Guide.pdf'] = open(pdf_url).read
+    mail(to: email, subject: 'Liam Howells Guide')
+  end
 end
