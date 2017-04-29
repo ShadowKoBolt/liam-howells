ba4e259830a649712c1920d6f0b3b687a1bd498d
Nicholas Bolt <nicholas.bolt@northgateps.com>
2017-04-29 21:22:26 +0100

    :wrench: Switch payment config to live

--- config/initializers/gocardless.rb
+++ config/initializers/gocardless.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 GocardlessClient = GoCardlessPro::Client.new(
-  access_token: ENV["GOCARDLESS_TOKEN"],
-  environment: :sandbox
+  access_token: ENV["GOCARDLESS_TOKEN"]
 )
